be0a3f0787d44baef5376b53e77e7d683ebb2144
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-15 14:49:52 -0300

    feat: animation variants added

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,14 +2,42 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '-100vw',
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: '0.5',
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw',
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 120,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div
-      initial={{ x: '-100vw' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.2, stiffness: 120 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
       className="base container"
     >
       <h3>Step 1: Choose Your Base</h3>
@@ -30,12 +58,7 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
-          className="next"
-        >
+        <motion.div variants={nextVariants} className="next">
           <Link to="/toppings">
             <motion.button
               whileHover={{
