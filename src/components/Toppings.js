c372062a299304ef48220d38e7d39e0b617f7a33
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-15 14:31:19 -0300

    feat: on hover animations added

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -18,15 +19,28 @@ const Toppings = ({ addTopping, pizza }) => {
         {toppings.map((topping) => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
+            <motion.li
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+              key={topping}
+              onClick={() => addTopping(topping)}
+            >
               <span className={spanClass}>{topping}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
 
       <Link to="/order">
-        <button>Order</button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: '0px 0px 8px rgb(255,255,255)',
+            boxShadow: '0px 0px 8px rgb(255,255,255)',
+          }}
+        >
+          Order
+        </motion.button>
       </Link>
     </div>
   );
