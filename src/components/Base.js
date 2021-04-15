ccf18b2d7da336a84f1b47f3ab992eba1c05f246
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-15 16:31:52 -0300

    feat: button loop animation added

--- src/components/Base.js
+++ src/components/Base.js
@@ -30,6 +30,18 @@ const nextVariants = {
   },
 };
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+    transition: {
+      duration: 0.4,
+      yoyo: Infinity,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thicc Crust'];
 
@@ -60,13 +72,7 @@ const Base = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div variants={nextVariants} className="next">
           <Link to="/toppings">
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: '0px 0px 8px rgb(255,255,255)',
-                boxShadow: '0px 0px 8px rgb(255,255,255)',
-              }}
-            >
+            <motion.button variants={buttonVariants} whileHover="hover">
               Next
             </motion.button>
           </Link>
