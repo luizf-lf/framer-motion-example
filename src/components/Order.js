3be12e6a20f65fa0cf2b228f282af9a7cc4c2966
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-16 09:39:01 -0300

    feat: modal to go back to home implemented

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const containerVariants = {
   hidden: {
@@ -28,7 +28,13 @@ const childVariants = {
   },
 };
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000);
+  }, [setShowModal]);
+
   return (
     <motion.div
       variants={containerVariants}
